04ba8d8f4ef6a4a32a63b4dd6aab81c02cb5df0a
Matthias Rueedlinger <md@rueedlinger.ch>
2025-04-17 20:48:54 +0200

    init

--- src/examples/example_diffusers.py
+++ src/examples/example_diffusers.py
@@ -1,19 +1,34 @@
-from diffusers import DiffusionPipeline
 import torch
-
-#generator = torch.Generator("cpu").manual_seed(0)
-
-# Use CPU instead of CUDA
-model_id = "stable-diffusion-v1-5/stable-diffusion-v1-5"
-pipeline = DiffusionPipeline.from_pretrained(model_id, use_safetensors=True)
-#pipeline = DiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16, use_safetensors=True)
-
-#pipeline.to("cuda")
-pipeline.to("cpu")
-#pipeline.to("mps")
+from diffusers import StableDiffusion3Pipeline
 
 
-image = pipeline("bear fishing in the mountains", guidance_scale=7.5, num_inference_steps=80, height=512, width=768).images[0]
+# Set device
+device = "mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu"
+
+# Load the pipeline
+model_id = "stabilityai/stable-diffusion-3-medium-diffusers"
+#model_id = "stabilityai/stable-diffusion-3.5-large"
+
+pipe = StableDiffusion3Pipeline.from_pretrained(model_id).to(device)
+
+
+# Set up a generator for reproducibility
+generator = torch.Generator(device=device).manual_seed(42)
+
+# Run the pipeline, showing some of the available arguments
+pipe_output = pipe(
+    #prompt="Palette knife painting of an autumn cityscape",  # What to generate
+    #negative_prompt="Oversaturated, blurry, low quality",  # What NOT to generate
+    prompt="Reealstic portrait of swiss mountain farmer",
+    #height=480,
+    #width=640,  # Specify the image size
+    guidance_scale=8,  # How strongly to follow the prompt
+    num_inference_steps=35,  # How many steps to take
+    generator=generator,  # Fixed random seed
+)
+
+# View the resulting image
+image = pipe_output.images[0]
 
 # Save image to disk
 image.save("test.png")
